090d6b3e34b19ed032538b2fc4969cdfb7a6593d
Jason Jarrett <jason@elegantcode.com>
2013-10-14 20:19:50 -0700

    break out some files

--- WinmdToTypeScript.Tests/Spike.cs
+++ WinmdToTypeScript.Tests/Spike.cs
@@ -19,36 +19,6 @@ namespace WinmdToTypeScript.Tests
 
     }
 
-    public class TypeTests : TestBase
-    {
-        [Test]
-        public void EnumType()
-        {
-            var result = GetNativeType("SampleEnum").ToTypeScript();
-            Approvals.Verify(result);
-        }
-
-        [Test]
-        public void EnumTypeWithExplicitValues()
-        {
-            var result = GetNativeType("SampleEnumNumbered").ToTypeScript();
-            Approvals.Verify(result);
-        }
-
-        [Test]
-        public void ClassWithEventHandler()
-        {
-            var result = GetNativeType("ClassWithEventHandler").ToTypeScript();
-            Approvals.Verify(result);
-        }
-
-        [Test]
-        public void FullAssembly()
-        {
-            var result = WinmdToTypeScript.Render.FullAssembly(base.NativeComponentPath);
-            Approvals.Verify(result);
-        }
-    }
 
     public static class Extensions
     {
---
+++ WinmdToTypeScript.Tests/TypeTests.cs
@@ -0,0 +1,42 @@
+﻿using ApprovalTests;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinmdToTypeScript.Tests
+{
+
+    public class TypeTests : TestBase
+    {
+        [Test]
+        public void EnumType()
+        {
+            var result = GetNativeType("SampleEnum").ToTypeScript();
+            Approvals.Verify(result);
+        }
+
+        [Test]
+        public void EnumTypeWithExplicitValues()
+        {
+            var result = GetNativeType("SampleEnumNumbered").ToTypeScript();
+            Approvals.Verify(result);
+        }
+
+        [Test]
+        public void ClassWithEventHandler()
+        {
+            var result = GetNativeType("ClassWithEventHandler").ToTypeScript();
+            Approvals.Verify(result);
+        }
+
+        [Test]
+        public void FullAssembly()
+        {
+            var result = WinmdToTypeScript.Render.FullAssembly(base.NativeComponentPath);
+            Approvals.Verify(result);
+        }
+    }
+}
