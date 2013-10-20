41a7a391c9e4121fd9d4e2324cf6a6b33a0d7389
Jason Jarrett <jason@elegantcode.com>
2013-10-20 10:53:31 -0700

    cleanup enum tests

--- ToTypeScriptD.Tests.JavascriptTests/specs/spec.Enums.ts
+++ ToTypeScriptD.Tests.JavascriptTests/specs/spec.Enums.ts
@@ -1,16 +1,30 @@
 ﻿/// <reference path="../definitions.ts" />
-
 var expect = chai.expect;
 
 describe('Enums', function () {
+    describe("when calling a method with an enum parameter.", function () {
 
-    it("when calling a method with an enum parameter.", function (done) {
-        var item = new ToTypeScriptD.Native.SampleEnumClass();
+        var result: ToTypeScriptD.Native.SampleEnum;
+        var input = ToTypeScriptD.Native.SampleEnum.b;
 
-        var result = item.methowWithEnumParameter(ToTypeScriptD.Native.SampleEnum.a);
-        expect(result).to.equal(ToTypeScriptD.Native.SampleEnum.a);
+        before(function () {
+            var enumClass = new ToTypeScriptD.Native.SampleEnumClass();
+            result = enumClass.methowWithEnumParameter(input);
+        });
 
-        expect(result.a).to.equal(ToTypeScriptD.Native.SampleEnum.a);
+        it("Should return the same enum value", function () {
+            expect(result).to.equal(input);
+
+            // This is odd TypeScript - the method return type of Enum and now typescript things I can take that result and pull properties off of it.
+            // Reported: https://typescript.codeplex.com/workitem/1817
+            expect(result.b).to.equal(undefined);
+        });
+
+        it("Should not get a value where the return type was an enum (since that was a number)", function () {
+            // This is odd TypeScript - the method return type of Enum and now typescript things I can take that result and pull properties off of it.
+            // Reported: https://typescript.codeplex.com/workitem/1817
+            expect(result.b).to.equal(undefined);
+        });
+
     });
-
 });
