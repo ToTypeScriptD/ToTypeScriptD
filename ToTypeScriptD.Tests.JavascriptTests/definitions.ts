8fbedfd88852c1750804a9a9b642a0f16e95d3ce
Jason Jarrett <jason@elegantcode.com>
2013-10-21 22:44:14 -0700

    TypeScript enable the WinJS test project

--- ToTypeScriptD.Tests.JavascriptTests/definitions.ts
+++ ToTypeScriptD.Tests.JavascriptTests/definitions.ts
@@ -1,6 +1,4 @@
 ﻿/// <reference path="Scripts/typings/ToTypeScriptD.Native/ToTypeScriptD.Native.d.ts" />
-/// <reference path="Scripts/typings/ToTypeScriptD.specialTypes.d.ts" />
 /// <reference path="Scripts/typings/chai/chai-assert.d.ts" />
 /// <reference path="Scripts/typings/chai/chai.d.ts" />
 /// <reference path="Scripts/typings/mocha/mocha.d.ts" />
-/// <reference path="Scripts/typings/Windows.Foundation.d.ts" />
