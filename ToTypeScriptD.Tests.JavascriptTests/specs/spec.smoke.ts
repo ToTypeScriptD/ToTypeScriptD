75fa2278bf9f0984995ab99984b96419edcee1e7
Jason Jarrett <jason@elegantcode.com>
2013-11-02 20:21:37 -0700

    turn smoke tests into ts. Also updae js project file to nest js files under ts

---
+++ ToTypeScriptD.Tests.JavascriptTests/specs/spec.smoke.ts
@@ -0,0 +1,19 @@
+﻿/// <reference path="../definitions.ts" />
+var expect = chai.expect;
+
+describe('a smoke tests', function () {
+
+    it("should pass", function () {
+
+    });
+
+    //it("should fail", function () {
+    //    equals(false, " oops ");
+    //});
+
+
+    it("should load the C++ component", function () {
+        //expect(ToTypeScriptD.Native).to.
+    });
+
+});
