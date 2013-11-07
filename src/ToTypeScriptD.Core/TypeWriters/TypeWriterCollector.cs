4cb195c65b2cf04b3e914eaf59237388c2aad077
Jason Jarrett <jason@elegantcode.com>
2013-11-06 22:18:57 -0800

    cleaned up some usings

--- src/ToTypeScriptD.Core/TypeWriters/TypeWriterCollector.cs
+++ src/ToTypeScriptD.Core/TypeWriters/TypeWriterCollector.cs
@@ -1,10 +1,7 @@
 ﻿using Mono.Cecil;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using ToTypeScriptD.Core.TypeWriters;
 
 namespace ToTypeScriptD.Core.TypeWriters
 {
