378146e2d6448f44cc1f80e7af59ac57b0c49424
Jason Jarrett <jason@elegantcode.com>
2016-12-23 16:02:25 -0800

    Can't even...
    step away for 3 years without everything going to shite...
    
    No idea what I'm doing here, but this gets the projects building - lots of
    work to do still - arggg

--- src/ToTypeScriptD.Native/ClassWithEventHandler.h
+++ src/ToTypeScriptD.Native/ClassWithEventHandler.h
@@ -8,16 +8,20 @@ namespace ToTypeScriptD
 
 		ref class ClassWithEventHandler;
 		public delegate void SomethingHappenedEventHandler(ClassWithEventHandler^ sender, Platform::String^ s, int foo);
-
+		
 		public ref class ClassWithEventHandler sealed
 		{
 		public:
 			ClassWithEventHandler();
-            event Windows::Foundation::TypedEventHandler<ClassWithEventHandler^, ToTypeScriptD::Native::SampleEnum>^ SampleTyped;
 			event SomethingHappenedEventHandler^ SomethingHappened;
 			void DoSomething();
 			void DoSomethingTyped();
 
+		internal:
+
+			event Windows::Foundation::TypedEventHandler<ClassWithEventHandler^, ToTypeScriptD::Native::SampleEnum>^ SampleTyped;
+
 		};
+
 	}
 }
